e476a12619043c616c956529e0a2ab1acd6b07a5
biwanshrestha14 <biwanshrestha77@gmail.com>
2025-02-28 19:17:23 +0545

    layouts

---
+++ layouts/app/(admin)/adminlogout/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <div>
+      i am a logout
+    </div>
+  )
+}
+
+export default Logout
---
+++ layouts/app/(admin)/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+import Navbar from "../components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: " Admin-Facebook-Connect with your friends",
+  description: " Admin-facebook is a simple and easy",
+};
+
+export default function AdminLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
